dcf69e31b2ec815cd7b38839335f389eb0999e72
nonam00 <raisky.ilya3@yandex.ru>
2023-05-25 20:37:12 +0300

    Добавьте файлы проекта.

---
+++ 02.06/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student;
+            try
+            {
+                student = new Student(Console.ReadLine(), Console.ReadLine(), Convert.ToInt32(Console.ReadLine()));
+                Console.WriteLine(student.ToString());
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ 02.06/Student.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._06
+{
+    internal class Student
+    {
+        private string first_name;
+        private string last_name;
+        private int age;
+
+        public Student(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return($"First name: {FirstName}\nLast name: {LastName}\nAge: {Age}\n\n");
+        }
+
+        public string FirstName
+        {
+            set
+            {
+                if(value == "")
+                    throw new Exception("Argument is empty");
+                else
+                    first_name = value;
+            }
+            get { return first_name; }
+        }
+        public string LastName
+        {
+            set
+            {
+                if (value == "")
+                    throw new Exception("Argument is empty");
+                else
+                    last_name = value;
+            }
+            get { return last_name; }
+        }
+        public int Age
+        {
+            set
+            {
+                if (value < 14)
+                    throw new Exception("Age is lower than 14");
+                else
+                    age = value;
+            }
+            get { return age; }
+        }
+        
+    }
+}
